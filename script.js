2c6b1ae909388e0a1c83dd8b9c8033afdd6645d0
xleoomelhor <107437806+xleoomelhor@users.noreply.github.com>
2024-08-26 14:53:44 -0300

    script.js

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você é a favor da COTA RACIAL?",
         alternativas: [
-            "Isso é assustador!",
-            "Isso é maravilhoso!"
+            "Sim, com certeza!",
+            "Não, um vexame!"
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Você acredita que o sistema de cotas é justo?",
         alternativas: [
-            "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-            "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+            "Sim, pois serve muito bem como uma inclusão e compensasão histórica.",
+            "Não! A Cota Racial demonstra inferioridade a certas etnias/raças",
         ]
     },
     {
