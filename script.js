d03eed3d87f51067d0ca14f5ee6f76da83e9abe4
xleoomelhor <107437806+xleoomelhor@users.noreply.github.com>
2024-08-26 15:06:10 -0300

    index

--- script.js
+++ script.js
@@ -16,28 +16,21 @@ const perguntas = [
         enunciado: "Você acredita que o sistema de cotas é justo?",
         alternativas: [
             "Sim, pois serve muito bem como uma inclusão e compensasão histórica.",
-            "Não! A Cota Racial demonstra inferioridade a certas etnias/raças",
+            "Não! A Cota Racial demonstra inferioridade a certas etnias/raças em inclusões sociais e profissionais, aonde pessoas ganham sem mérito próprio.",
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Para você, as Cotas para as universidade, irão ajudar a diminuir o racismo e dicriminação?",
         alternativas: [
-            "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-            "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+            "Sim, pois com essas oportunidade poderão incluir outras pessoas  a grandes instituições de ensino a fim de terem uma boa educação e consequentemente diminuir a discriminação.",
+            "Não. Mesmo tendo oportunidades para se adentrarem a grandes instituições de ensino, isso não diminuiria a dicriminação social, e também, de certo modo, poderia até aumentar a discriminação."
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Você utilizaria o sistema de Cotas Raciais?",
         alternativas: [
-            "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-            "Criar uma imagem utilizando um gerador de imagem de IA."
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
-        alternativas: [
-           "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-            "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+            "Sim, pois me beneficiaria bastante e justo.",
+            "Não, pois não me beneficiaria e/ou não acho muito justo."
         ]
     },
 ];
